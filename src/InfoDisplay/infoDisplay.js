90209a381a75c6a1aa6c0a9ea53fb27e2b4dc752
Austin Cumberlander <austincumberlander@gmail.com>
2018-12-13 21:04:24 -0600

    tried to create infoDisplay. code is now broken

--- src/InfoDisplay/infoDisplay.js
+++ src/InfoDisplay/infoDisplay.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './infoDisplay.scss';
+import {
+  ListGroup,
+  ListGroupItem,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import classnames from 'classnames';
+
 
 class InfoDisplay extends React.Component {
   render() {
     return (
       <div className="info-display">
-        <h2>Info Display</h2>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1' })}
+              onClick={() => { this.toggle('1'); }}
+            >
+              Tab1
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className= {active: this.state.activeTab === '2'} 
+              onClick={() => { this.toggle('2'); }}
+            >
+              Bio
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <ListGroup>
+          <h2>Info Display</h2>
+          <ListGroupItem>Item 1</ListGroupItem>
+          <ListGroupItem>Item 2</ListGroupItem>
+          <ListGroupItem>...</ListGroupItem>
+        </ListGroup>
       </div>
     );
   }
