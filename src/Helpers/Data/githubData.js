5bcd70c61b1ddc0fcf3a3b52f5a47a87ca9266ef
Austin Cumberlander <austincumberlander@gmail.com>
2019-01-06 18:46:33 -0600

    stuck

--- src/Helpers/Data/githubData.js
+++ src/Helpers/Data/githubData.js
@@ -1,34 +1,34 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 
-const getUser = token => new Promise((resolve, reject) => {
-  axios.get('https://api.github.com/user', { headers: { Authorization: `token ${token}` } })
-    .then((res) => {
-      const profile = res.data;
-      console.log(profile);
-      resolve(profile);
-    })
-    .catch((err) => {
-      reject(err);
-    });
-});
+// const getUser = token => new Promise((resolve, reject) => {
+//   axios.get('https://api.github.com/user', { headers: { Authorization: `token ${token}` } })
+//     .then((res) => {
+//       const profile = res.data;
+//       console.log(profile);
+//       resolve(profile);
+//     })
+//     .catch((err) => {
+//       reject(err);
+//     });
+// });
 
-const getUserEvents = (userName, token) => new Promise((resolve, reject) => {
-  axios.get(`https://api.github.com/users/${userName}/events/public`, { headers: { Authorization: `token ${token}` } })
-    .then((res) => {
-      let commitCount = 0;
-      const pushEvents = res.data.filter(event => event.type === 'PushEvent');
-      pushEvents.forEach((pushEvent) => {
-        commitCount += pushEvent.payload.commits.length;
-      });
-      resolve(commitCount);
-    })
-    .catch((err) => {
-      reject(err);
-    });
-});
+// const getUserEvents = (userName, token) => new Promise((resolve, reject) => {
+//   axios.get(`https://api.github.com/users/${userName}/events/public`, { headers: { Authorization: `token ${token}` } })
+//     .then((res) => {
+//       let commitCount = 0;
+//       const pushEvents = res.data.filter(event => event.type === 'PushEvent');
+//       pushEvents.forEach((pushEvent) => {
+//         commitCount += pushEvent.payload.commits.length;
+//       });
+//       resolve(commitCount);
+//     })
+//     .catch((err) => {
+//       reject(err);
+//     });
+// });
 
-export default {
-  getUser,
-  getUserEvents,
-};
+// export default {
+//   getUser,
+//   getUserEvents,
+// };
--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import './profile.scss';
+// import React, { Component } from 'react';
+// import './profile.scss';
 
-class Profile extends Component {
-  render() {
-    const { profile, commitCount } = this.props;
-    return (
-      <div className="profile col">
-        <div className="profileWrap">
-        <h2>Profile</h2>
-        <div className="card">
-          <img className="img-fluid" src={profile.avatar_url} alt="github pic"></img>
-          <p className="card-text">{profile.bio}</p>
-          <h2 className="card-title">{profile.login}</h2>
-          <a href={profile.html_url} className="_blank">https://github.com/{profile.login}</a>
-          <br/>
-          <br/>
-          <h6>Commits</h6>
-          <p>in the last 5 days</p>
-          <h4>{commitCount}</h4>
-        </div>
-        </div>
-      </div>
-    );
-  }
-}
+// class Profile extends Component {
+//   render() {
+//     const { profile, commitCount } = this.props;
+//     return (
+//       <div className="profile col">
+//         <div className="profileWrap">
+//         <h2>Profile</h2>
+//         <div className="card">
+//           <img className="img-fluid" src={profile.avatar_url} alt="github pic"></img>
+//           <p className="card-text">{profile.bio}</p>
+//           <h2 className="card-title">{profile.login}</h2>
+//           <a href={profile.html_url} className="_blank">https://github.com/{profile.login}</a>
+//           <br/>
+//           <br/>
+//           <h6>Commits</h6>
+//           <p>in the last 5 days</p>
+//           <h4>{commitCount}</h4>
+//         </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default Profile;
+// export default Profile;
